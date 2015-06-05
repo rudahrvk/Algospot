e831e23b15817c7ee592a307e37c28def10c1564
rudahrvk <byeon.kyeongmok@sualab.com>
2015-06-05 11:12:07 +0900

    nerd2 complete

--- AlgorithmTraining/4NERD2.cpp
+++ AlgorithmTraining/4NERD2.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <map>
+
+#include "Simulator.h"
 
 inline int getInt() 
 {
@@ -10,28 +13,81 @@ inline int getInt()
 	return s;
 }
 
-int pData[50000 * 2];
 void Nerd2()
 {
+	typedef std::map<int, int> RBTreeInt;
 	int numCase, numN;
 	numCase = getInt();
-	for (; numCase--;)
+	for (int c = 0; c < numCase; c++)
 	{
-		numN = getInt();
+ 		numN = getInt();
+		int sum = 0;
+		RBTreeInt treeData;
 		for (int i = 0; i < numN; i++)
 		{
-			pData[2 * i] = getInt();
-			pData[2 * i + 1] = getInt();
+			int curX = getInt();
+			int curY = getInt();
+			RBTreeInt::iterator itLowerBound = treeData.lower_bound(curX);
+			if (itLowerBound != treeData.end() && itLowerBound->second > curY)	// cannot be inserted
+			{
+				sum += treeData.size();
+				continue;
+			}
+
+			if (itLowerBound != treeData.begin())	// erase invalidated data
+			{
+				itLowerBound--;
+				RBTreeInt::iterator itTmp;
+				while (itLowerBound->second < curY)
+				{
+					if (itLowerBound == treeData.begin())
+					{
+						treeData.erase(itLowerBound);
+						break;
+					}
+					itTmp = itLowerBound;
+					itTmp--;
+					treeData.erase(itLowerBound);
+					itLowerBound = itTmp;
+				}
+			}
+
+			treeData[curX] = curY;	// insert cur data
+			sum += treeData.size();
 		}
-
-
-
+		printf("%d\n", sum);
 	}
 }
 
 int main()
 {
+// 	std::map<int, int> myMap;
+// 	myMap[1] = 1;
+// 	myMap[4] = 1;
+// 	myMap[7] = 1;
+// 	myMap[9] = 1;
+// 	std::map<int, int>::iterator it = myMap.lower_bound(8);
+// 	std::vector<std::pair<int, int>> myVec;
+// 	myVec.push_back(std::make_pair(72, 50));
+// 	myVec.push_back(std::make_pair(57, 67));
+// 	myVec.push_back(std::make_pair(74, 55));
+// 	myVec.push_back(std::make_pair(64, 60));
+//
+//	std::map<int, int> myMap;
+//	std::map<std::pair<int, int>, int> myMap;
+//	myMap.insert(myVec.begin(), myVec.end());
+// 
+// 	myMap[std::make_pair(72, 50)] = 1;
+// 	myMap[std::make_pair(57, 67)] = 1;
+// 	myMap[std::make_pair(74, 55)] = 1;
+// 	myMap[std::make_pair(64, 60)] = 1;
+// 	std::map<std::pair<int, int>, int>::iterator itlow = myMap.lower_bound(std::make_pair(70, 58));
+
+// 	Simulator<int, int> mySimulator(pData, 50, 50000, 2, 1);
+// 	mySimulator.Generator(0, 100000);
+// 	mySimulator.CompareResult(&Nerd2, &Nerd2);
+
 	Nerd2();
-	system("pause");
+ 	system("pause");
 	return 0;
 }
---
+++ AlgorithmTraining/Simulator.h
@@ -0,0 +1,92 @@
+#include <random>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+#include "Clock.h"
+
+template <class InputType, class OutputType>
+class Simulator
+{
+public:
+	Simulator(InputType* pData, int numCase, int arrHeight, int arrWidth, int lenOutput)
+	{
+		m_pData = pData;
+		m_numCase = numCase;
+		m_arrHeight = arrHeight;
+		m_arrWidth = arrWidth;
+		m_lenOutput = lenOutput;
+	}
+
+	~Simulator()
+	{
+	}
+
+	void Generator(InputType minVal, InputType maxVal)
+	{
+		std::random_device randDev;
+		std::mt19937 generator(randDev());
+		std::uniform_real_distribution<float> distVal(0, 1);
+		InputType interval = maxVal - minVal;
+
+		for (int i = 0; i < m_numCase * m_arrWidth * m_arrHeight; i++)
+		{
+			m_pData[i] = (InputType)(distVal(generator) * interval + minVal);
+		}
+	}
+
+	void CompareResult(void(*pFunc1)(void), void(*pFunc2)(void))
+	{
+		Clock myClock;
+		myClock.MeasureBegin();
+		std::stringstream ss1, ss2;
+		auto oldCoutBuf = std::cout.rdbuf(ss1.rdbuf());		//redirect to ss1
+		pFunc1();
+		myClock.MeasureEnd();
+		
+		double t1 = myClock.GetSectionTime();
+
+		std::cout.rdbuf(ss2.rdbuf());	//redirect to ss2
+		pFunc2();
+		
+		std::cout.rdbuf(oldCoutBuf);	//restore original cout
+
+		std::cout << "time : " << t1 << "\n";
+
+// 		for (int c = 0; c < m_numCase; c++)
+// 		{
+// 			for (int l = 0; l < m_lenOutput; l++)
+// 			{
+// 				OutputType tmp1, tmp2;
+// 				ss1 >> tmp1;
+// 				ss2 >> tmp2;
+// 				if (tmp1 != tmp2)
+// 				{
+// 					std::cout << "different case occurs\n";
+// 					InputType* curData = pData + c * m_arrWidth * m_arrHeight;
+// 					for (int row = 0; row < m_arrHeight; row++)
+// 					{
+// 						for (int col = 0; col < m_arrWidth; col++)
+// 						{
+// 							std::cout << curData[m_arrWidth * row + col] << " ";
+// 						}
+// 						std::cout << "\n";
+// 					}
+// 
+// 					c = m_numCase;	//exit double loop
+// 					break;
+// 				}
+// 			}
+// 		}
+// 
+// 		std::cout << "all results are the same\n";
+	}
+
+private:
+	int m_numCase;
+	int m_arrWidth;
+	int m_arrHeight;
+	int m_lenOutput;
+
+	InputType* m_pData;
+};
